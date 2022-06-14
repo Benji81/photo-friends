c60d983583f656282ce41b9ac138fc3af9ce4bcb
Benjamin <benji81@gmail.com>
2022-06-14 22:29:01 +0200

    fix(docker): simplify entrypoint and listen to 127.0.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ export VENV_PATH=/opt/venv
 export PATH="$POETRY_PATH/bin:$VENV_PATH/bin:$PATH"
 
 cd /var/www/photofriends
-if [ $INIT == "1" ]; then
-  poetry run python manage.py migrate
-  poetry run python manage.py collectstatic --noinput
-fi
+poetry run python manage.py migrate
+poetry run python manage.py collectstatic --noinput
 poetry run $*
